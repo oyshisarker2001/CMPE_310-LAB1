9ba68fbdde28d1bc0de782ea7141b08a77661aa1
Oyshi Pronomy Sarker <57029168+oyshisarker2001@users.noreply.github.com>
2026-09-10 02:42:49 +0000

    commit

---
+++ task_1.s
@@ -0,0 +1,23 @@
+.section .bss
+.global ram
+
+ram:
+    .zero 256
+
+
+.section .text
+.global fill_ram
+
+fill_ram:
+
+    movb $0xFF, ram+0x50
+    movb $0xFF, ram+0x51
+    movb $0xFF, ram+0x52
+    movb $0xFF, ram+0x53
+    movb $0xFF, ram+0x54
+    movb $0xFF, ram+0x55
+    movb $0xFF, ram+0x56
+    movb $0xFF, ram+0x57
+    movb $0xFF, ram+0x58
+
+    ret
